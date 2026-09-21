9f16767473740e8f4830ed92122df62194e3c13a
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-09-22 00:03:34 +0200

    Comment out documentation page also

--- community/make_site.py
+++ community/make_site.py
@@ -567,26 +567,26 @@ with (DATA/'theories_index.yaml').open('r', encoding='utf-8') as h_file:
 #             setattr(course, field, render_markdown("\n".join(map(lambda v: "* " + v, val))))
 # courses_tags = ['lean4', 'lean3'] + sorted(list(courses_tags))
 
-documentation_tags = {}
-documentation_lists = {
-        'tutorial': [],
-        'how-to': [],
-        'explanation': [],
-        'reference': [],
-}
-with (DATA/'documentation.yaml').open('r', encoding='utf-8') as file:
-    docu_data = yaml.safe_load(file)
-    for e in docu_data["tags"]:
-        documentation_tags[e["name"]] = DocumentationTag(**e)
+# documentation_tags = {}
+# documentation_lists = {
+#         'tutorial': [],
+#         'how-to': [],
+#         'explanation': [],
+#         'reference': [],
+# }
+# with (DATA/'documentation.yaml').open('r', encoding='utf-8') as file:
+#     docu_data = yaml.safe_load(file)
+#     for e in docu_data["tags"]:
+#         documentation_tags[e["name"]] = DocumentationTag(**e)
 
-    for e in docu_data["documentation"]:
-        e = DocumentationEntry(**e)
-        e.description = render_markdown(e.description)
-        documentation_lists[e.category].append(e)
-        for tag in e.tags:
-            documentation_tags[tag].count += 1
-            if not documentation_tags[tag].display:
-                e.display = False
+#     for e in docu_data["documentation"]:
+#         e = DocumentationEntry(**e)
+#         e.description = render_markdown(e.description)
+#         documentation_lists[e.category].append(e)
+#         for tag in e.tags:
+#             documentation_tags[tag].count += 1
+#             if not documentation_tags[tag].display:
+#                 e.display = False
 
 # Cannot use %-d format code on windows
 def format_month_day(date_obj):
@@ -960,7 +960,7 @@ def render_site(target: Path, base_url: str, edit_base: str = DEFAULT_EDIT_BASE,
                 # ('events.html', {'old_events': old_events, 'new_events': new_events}),
                 # ('teaching/courses.html', {'courses': courses, 'tags': courses_tags}),
                 # ('teams.html', {'introduction': read_md('teams_intro.md'), 'teams': teams}),
-                ('documentation.html', {'documentation_lists': documentation_lists, 'documentation_tags': documentation_tags}),
+                # ('documentation.html', {'documentation_lists': documentation_lists, 'documentation_tags': documentation_tags}),
                 ('.*.md', get_contents),
                 ],
             filters={ 'url': url, 'md': render_markdown, 'tex': clean_tex },
